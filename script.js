1dd0eb8f32e8e17a1d27f6050bd879dcf762c22b
jmolina518 <jmolina518@gmail.com>
2021-02-19 20:37:45 -0600

    run checkBoxes function

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const boxes = document.querySelectorAll('.box')
 
 window.addEventListener('scroll', checkBoxes)
 
+checkBoxes()
+
 function checkBoxes() {
   const triggerBottom = (window.innerHeight / 5) * 4
 
